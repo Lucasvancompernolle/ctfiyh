4f6a1ddce3a9b0be81964201f5f42d55d424a3c3
lucas784 <lucas.van.compernolle@tvh.com>
2025-07-13 21:18:53 +0200

    first commit

---
+++ src/test/java/com/ctfiyh/restaurant/restaurant/RestaurantApplicationTests.java
@@ -0,0 +1,15 @@
+package com.ctfiyh.restaurant.restaurant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class RestaurantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
